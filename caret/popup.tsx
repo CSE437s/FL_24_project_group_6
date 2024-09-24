552fb9a031707533ae984310280447e11fa1537a
souravm1230 <m.sourav@wustl.edu>
2024-09-24 17:55:19 -0500

    Created a basic Login template

--- caret/popup.tsx
+++ caret/popup.tsx
@@ -6,20 +6,52 @@ function IndexPopup() {
   return (
     <div
       style={{
-        padding: 16
+        padding: 40,
+        width: 340
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+
+      <h1 style={{textAlign: "center"}}>Welcome to Caret</h1>
+      <h3 style={{textAlign: "center"}}>Comments for the Internet</h3>
+
+      <input 
+        type="text" 
+        placeholder="Username" 
+        style={{
+          width: "93%",
+          padding: 10,
+          marginTop: 20
+        }}
+      />
+
+      <input 
+        type="password" 
+        placeholder="Password" 
+        style={{
+          width: "93%",
+          padding: 10,
+          marginTop: 20
+        }}
+      />
+
+      <button
+        style={{
+          width: "100%",
+          padding: 10,
+          marginTop: 20,
+          backgroundColor: "green",
+          color: "white",
+          // add a shadow effect to the button
+          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+          // make the button look clickable
+          cursor: "pointer"
+        }}
+      >
+        Login 
+
+      </button>
+        
     </div>
+
   )
 }
 
