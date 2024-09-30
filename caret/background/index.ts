d28972b2bf8d690c28de00c63ce307e81e5d1138
noahg957 <noahgluck@gmail.com>
2024-09-30 01:32:13 -0500

    add mvp functionality

--- caret/background/index.ts
+++ caret/background/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { get_url_comments } from "~api"
 
 // this is the logic to add a create caret to the right click menu when the extension is installed
 chrome.runtime.onInstalled.addListener(function () {
@@ -15,11 +16,28 @@ chrome.contextMenus.onClicked.addListener(async function (info, tab) {
   chrome.tabs.sendMessage(tab.id, {
       action: "show_comment_create"
     })
-    // chrome.tabs.sendMessage(tab.id, {
-    //     action: "get_css_selector"
-    // }, (response) => {
-    //   console.log("response")
-    // })
 })
 
-console.log("hello")
+// //this logic runs whenever a user goes to a new url
+// chrome.tabs.onUpdated.addListener(
+//   async function(tabId, changeInfo, tab) {
+//     if (changeInfo.status === 'complete') {
+//       //get all the comments that are on that url
+//       console.log("url changed")
+//       await new Promise(r => setTimeout(r, 1000));
+//       try {
+//         let comments = await get_url_comments(changeInfo.url)
+//         chrome.tabs.sendMessage(tabId, {
+//           action: "display_comments_on_url",
+//           comments: comments.data
+//         })
+//         console.log("data sent")
+//       }
+//       catch {
+//         console.log("comment fetch failed")
+//       }
+      
+      
+//     }
+//   }
+// )
---
+++ caret/background/messages/get_url_comments.ts
@@ -0,0 +1,14 @@
+import type { PlasmoMessaging } from "@plasmohq/messaging"
+import { get_url_comments } from "~api" 
+ 
+const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
+  console.log(req)
+  let response = await get_url_comments(req.body.url)
+  console.log(response)
+  res.send({
+    message: "display_comments",
+    comments: response.data 
+  })
+}
+ 
+export default handler
