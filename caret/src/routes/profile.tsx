d329c4e6b6fe2b22d9987c494b0000454c0ea72f
SophieSchochet <87827703+SophieSchochet@users.noreply.github.com>
2024-11-05 12:25:00 -0600

    profile UI

--- caret/src/routes/profile.tsx
+++ caret/src/routes/profile.tsx
@@ -7,6 +7,11 @@ export const Profile = ({ user, setIsLoggedIn}) => {
   const storage = new Storage({
     copiedKeyList: ["shield-modulation"],
   });
+  const colors = ["#C8DB2A", "#FF7BAD", "6FE4CC", "#185D79", "#EF4686"]; //assign user color from here and put in circle
+  let strValue: string = user as string;
+  const letter = strValue.charAt(0);//first letter
+
+  const followers = 0;// get followers and this number would change, const for now
 
   const handleHomeClick = () => {
     navigate("/home");
@@ -20,16 +25,16 @@ export const Profile = ({ user, setIsLoggedIn}) => {
   };
 
   return (
-    <div className="flex flex-col justify-center p-5">
-      <h1 className="text-2xl font-semibold mb-6">{user} Profile</h1>
-
-      <button
-        onClick={handleHomeClick}
-        className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 mb-4"
-      >
-        Home
-      </button>
-
+    <div className=" w-full flex flex-col justify-center p-5">
+      <div className="w-full flex items-start m-3">
+        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-[#FF7BAD]"> 
+    <p className="text-xl">{letter}</p>
+  </div>
+  <div className = "flex flex-col ml-2">
+  <p className="text-2xl font-bold">{user}</p>
+  <p className = "text-sm  text-gray-400">{followers} followers</p>
+  </div>
+  </div>
       <button
         onClick={handleLogout}
         className="w-full bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600"
